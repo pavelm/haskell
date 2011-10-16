a9697d69295a022b0bc8131dc800a98f92b1decd
Pavel Margolin <pmargolin@gmail.com>
2011-10-15 23:08:03 -0400

    routing

--- happstack/hello.hs
+++ happstack/hello.hs
@@ -1,6 +1,15 @@
 module Main where 
 
-import Happstack.Server (nullConf, simpleHTTP, toResponse, ok)
+import Control.Monad (msum)
+import Data.Char (toLower)
+import Happstack.Server (Method(GET, POST), methodM, nullConf, simpleHTTP, dir, ok, path)
 
 main :: IO ()
-main = simpleHTTP nullConf $ ok "Hello, World!"
+main = simpleHTTP nullConf $ msum
+       [ do methodM GET 
+            ok $ "You did a GET request.\n"
+       , do methodM POST
+            ok $ "You did a POST request.\n"
+       , dir "foo" $ do methodM GET
+                        ok $ "You did a GET request on /foo\n"
+       ]
